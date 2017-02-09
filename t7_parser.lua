b6afe403dc9e50c25bf252a87c8cfdfe28e35fd5
Pradeep Sakhamoori <pradeep.sakhamoori@intel.com>
2017-02-09 11:04:18 -0800

    Torch Model file (.t7) parser

---
+++ t7_parser.lua
@@ -0,0 +1,21 @@
+--[[
+Purpose:							       
+  To parse the torch pre-trained model file (t7)                      
+  Prints layer, kernel size, stride and padding information           
+How to use							       			
+  Replace nn4.samll2.v1.t7 with any of the torch pre-trained trained   
+  model file (t7) 						       
+Pre-requisites:						       
+  Machine with lua, torch and supporting Luarocks isntalled           
+ Ref: www.torch.ch on how to install                                  
+]]--
+
+require 'nn'
+require 'dpnn'
+
+model = torch.load('nn4.small2.v1.t7')
+for i = 1, #model.modules do
+    print(model.modules[i])
+end
+
+
